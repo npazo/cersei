459fb04c0bf382fce17cc003b8e84e80c7cb9aed
Nick Pazoles <np7776@gmail.com>
2015-06-14 19:02:42 -0500

    add setting to disable new account creation closes #3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :media
 
-  devise_for :users
-  
+   devise_for :users, controllers: { registrations: "users/registrations" }
+ 
     resources :settings do
     collection do
       get 'mark_all_reviewed'
